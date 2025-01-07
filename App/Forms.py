36d64921727db8764a03d1c91273eb9411a591fc
alonandreson <alonandreson@gmail.com>
2025-01-07 18:58:11 +0200

    Merge remote-tracking branch 'origin/main'

--- App/Forms.py
+++ App/Forms.py
@@ -1,1 +1,4 @@
 ## class that used for checking login credentials and sign up methods
+
+
+    def __init__(self):
--- App/Models.py
+++ App/Models.py
@@ -1,7 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
 
 
-class Item
+class Item:
+    #הורדת מוצר כשמלאי=0 מתצוגה
+    #הכנסת פריט
+    #בדיקה אם קיים קמפיין
+
+
+class User:
+        # מודא קיום בטבלה
+        # רישום
+        # רכישה
+
+class Cart:
+    # הוספה לעגלה (מחיר,סכימה של מחירים,פריט לרשימה)
+    # הורדת פריט
+
+class Purchase:
+    # בדיקת מלאי DB
+    # עדכון DB
+
     #building DB columns
 
 # function of init
